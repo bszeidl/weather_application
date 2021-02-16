a9abfe37ba36c3ffdc5c04413ef266fff3631c28
Bian Péter <bianpeti@gmail.com>
2021-02-16 15:43:00 +0100

    p-branch

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,18 +1,43 @@
 import "./scss/main.scss";
-import { useState } from "react";
-import { useWeather } from "./components/fetch";
+import React, { useState } from "react";
+//import { useWeather } from "./components/fetch";
+
+const API = {
+  key: "b0a23f6e90480e33936456139644bff5",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  /*
   const [newCity, setNewCity] = useState();
   const city = useWeather(newCity);
 
   const changeCity = (event) => {
     setNewCity(event.target.value);
   };
-
+*/
   return (
     <div className="App">
-      <label htmlFor="cities">Choose a city:</label>
+      <div className="main">
+        <div className="input-container">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="location-container">
+          <div className="location">Budapest, HU</div>
+          <div className="humidity"></div>
+        </div>
+      </div>
+  
+
+
+
+      {/*
+     <label htmlFor="cities">Choose a city:</label>
 
       <select name="cities" id="cities" onChange={changeCity}>
         <option value="London">London</option>
@@ -26,6 +51,7 @@ function App() {
           ? "Placeholder text"
           : `Temp: ${city.list[0].main.temp} ℃`}
       </div>
+      */}
     </div>
   );
 }
