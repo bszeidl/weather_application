82edeed4b62e3e24727cf16489b2836baf2828df
TamasKrisztian <kortanaz@gmail.com>
2021-02-16 13:49:18 +0100

    fetch

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,31 @@
 import "./scss/main.scss";
+import { useState } from "react";
+import { useWeather } from "./components/fetch";
 
 function App() {
+  const [newCity, setNewCity] = useState();
+  const city = useWeather(newCity);
+
+  const changeCity = (event) => {
+    setNewCity(event.target.value);
+  };
+
   return (
     <div className="App">
-      <div></div>
+      <label htmlFor="cities">Choose a city:</label>
+
+      <select name="cities" id="cities" onChange={changeCity}>
+        <option value="London">London</option>
+        <option value="Budapest">Budapest</option>
+        <option value="Köln">Köln</option>
+        <option value="Sydney">Sydney</option>
+        <option value="Detroit">Detroit</option>
+      </select>
+      <div>
+        {city === "Loading..."
+          ? "Placeholder text"
+          : `Temp: ${city.list[0].main.temp} ℃`}
+      </div>
     </div>
   );
 }
---
+++ weather-app/src/components/fetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (initUrl) => {
+  const [url, setUrl] = useState(initUrl);
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status !== 200) return "There must be a problem";
+        return response.json();
+      })
+      .then((json) => setData(json));
+  }, [url]);
+
+  return [data, setUrl];
+};
+
+export const useWeather = (city = "London") => {
+  const [weather, setUrl] = useFetch(
+    `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=7cf1f1b47542d9ecccf08ca82e87ec23`
+  );
+  useEffect(() => {
+    setUrl(
+      `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=7cf1f1b47542d9ecccf08ca82e87ec23`
+    );
+  }, [city]);
+  return weather === undefined ? "Loading..." : weather;
+};
