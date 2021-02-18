23ad4dd3196467c3d7dfe1ecdfaa5a2304cb4bd4
Bian Péter <bianpeti@gmail.com>
2021-02-18 16:47:08 +0100

    adding math.round to temperature output

--- weather-app/src/components/Output/Output.jsx
+++ weather-app/src/components/Output/Output.jsx
@@ -7,7 +7,7 @@ function Output({cityOut, inputValue}) {
           <h2>Press Enter</h2>
         ) : (
           <div className="output">
-            <h3 className="temp"> {cityOut.list[0].main.temp} ℃ </h3>
+            <h3 className="temp"> {Math.round(cityOut.list[0].main.temp)} ℃ </h3>
             <h3> {cityOut.list[0].weather[0].main}</h3>
             <h3> Humidity: {cityOut.list[0].main.humidity}%</h3>
           </div>
